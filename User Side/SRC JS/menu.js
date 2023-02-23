6fe279b6325c48a867b5d7c6774b5e676a42fb3c
izhar100 <ezharashraf523@gmail.com>
2023-02-23 12:34:40 +0530

    local storage working fine now pushing the code in day 3 branch

--- User Side/SRC JS/menu.js
+++ User Side/SRC JS/menu.js
@@ -40,49 +40,67 @@ function fetchdata(pageNumber){
 let products=document.getElementById('products')
 function displaydata(data){
     let count=data.length;
-    
-    let cardlists=`
-    ${
-        data.map((item)=>(
-            getcard(
-                item.image1,
-                item.image2,
-                item.brand,
-                item.name,
-                item.price
-            )
-        )).join("")
-    }
-    `;
-   
-    products.innerHTML=cardlists;
+    products.innerHTML="";
+
+    //product append
+
+    data.map((item,index)=>{
+        let div1=document.createElement("div")
+        let divheart=document.createElement("div")
+        divheart.setAttribute("id","heart")
+        let heartimg=document.createElement("img")
+        heartimg.src="https://cdn-icons-png.flaticon.com/512/14/14949.png?w=740&t=st=1676993626~exp=1676994226~hmac=27ebdcbb39c748208d4449aaa3f26253fae641221cbcee5eb68deea33ebae884";
+        divheart.append(heartimg);
+        let cardimg=document.createElement("div");
+        cardimg.setAttribute("id","img1");
+        let cardimage1=document.createElement("img")
+        cardimage1.setAttribute("class","firstimage")
+        cardimage1.src=item.image1;
+        cardimg.append(cardimage1);
+        let proinfo=document.createElement("div")
+        proinfo.setAttribute("id","proinfo")
+        let h2=document.createElement("h2")
+        h2.textContent="SALE";
+        let h3=document.createElement("h3")
+        h3.textContent=item.brand;
+        let protitle=document.createElement("p")
+        protitle.setAttribute("id","protitle")
+        protitle.setAttribute("class","producttitle")
+        protitle.textContent=item.name;
+        protitle.addEventListener("click",()=>{
+            productdata=item;
+            localStorage.setItem("product",JSON.stringify(productdata))
+        })
+
+        let prices=document.createElement("div")
+        prices.setAttribute("id","prices")
+        let price1=document.createElement("p")
+        price1.setAttribute("id","price1")
+        price1.textContent="$"+item.price+".99";
+        prices.append(price1)
+        let rating=document.createElement("div")
+        rating.setAttribute("id","rating")
+        let star1=document.createElement("i")
+        star1.setAttribute("class","fa-solid fa-star")
+        let star2=document.createElement("i")
+        star2.setAttribute("class","fa-solid fa-star")
+        let star3=document.createElement("i")
+        star3.setAttribute("class","fa-solid fa-star")
+        let star4=document.createElement("i")
+        star4.setAttribute("class","fa-solid fa-star")
+        let star5=document.createElement("i")
+        star5.setAttribute("class","fa-regular fa-star-half-stroke")
+        rating.append(star1,star2,star3,star4,star5)
+        let coupon=document.createElement("p")
+        coupon.textContent="Coupon Excluded";
+        proinfo.append(h2,h3,protitle,prices,rating)
+        div1.append(divheart,cardimg,proinfo,coupon)
+        products.append(div1)
+    })
+
+    //product append end
 }
-function getcard(image1,image2,brand,name,price){
-    let card=`<div>
-    <div id="heart">
-    <img src="https://cdn-icons-png.flaticon.com/512/14/14949.png?w=740&t=st=1676993626~exp=1676994226~hmac=27ebdcbb39c748208d4449aaa3f26253fae641221cbcee5eb68deea33ebae884">
-    </div>
-    <div id="img1">
-        <img src="${image1}"
-            alt="" class="firstimage">
-    </div>
-    <div id="proinfo">
-        <h2>SALE</h2>
-        <h3>${brand}</h3>
-        <p id="protitle" class="producttitle">${name}</p>
-        <div id="prices"><p id="price1">$${price}.99</p></div>
-        <div id="rating">
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star-half-stroke"></i>
-        </div>
-        <p>Coupon Excluded</p>
-    </div>
-</div>`;
-return card;
-}
+
 let pages=document.getElementById("pages")
 
 
@@ -153,13 +171,3 @@ function showpages(total,limit){
   })
 
   //end filter code
-
-  setTimeout(function(){
-    let producttitle=document.querySelectorAll(".producttitle")
-    console.log(producttitle)
-    for(let i=0; i<producttitle.length; i++){
-        producttitle[i].addEventListener("click",()=>{
-            localStorage.setItem("product",JSON.stringify(alldata[i]))
-        })
-    }
-  },1000)
