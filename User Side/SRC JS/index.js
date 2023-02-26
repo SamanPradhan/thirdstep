c0b2fe6a06c471280b80d2b563b50913ad1d790b
izhar100 <ezharashraf523@gmail.com>
2023-02-26 21:21:13 +0530

    final commit

--- User Side/SRC JS/index.js
+++ User Side/SRC JS/index.js
@@ -2,3 +2,16 @@ let loginUserToken = localStorage.getItem("loginUser") || false;
 console.log("loginUserToken:", loginUserToken);
 let login_name = JSON.parse(localStorage.getItem("login_name")) || [];
 let cartData = JSON.parse(localStorage.getItem("productsAdd")) || [];
+
+//search functionality
+let searchquery=document.getElementById("searchquery")
+let searchbtn=document.getElementById("searchbtn")
+//cursor pointer css using js
+searchbtn.addEventListener("mouseover",()=>{
+    searchbtn.style.cursor="pointer";
+})
+//cursor pointer css end
+searchbtn.addEventListener("click",()=>{
+    localStorage.setItem("searchValue",searchquery.value)
+    window.location.href="menu.html";
+})
